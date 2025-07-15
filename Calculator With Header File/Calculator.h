33f983c1c202a66985e962bf88a6f6e2944a86b9
Rohit Vadher <vadherrohit239@gmail.com>
2025-07-15 09:16:27 +0530

    Update Calculator.h

--- Calculator With Header File/Calculator.h
+++ Calculator With Header File/Calculator.h
@@ -1,3 +1,16 @@
 #include<iostream>
 
-void calculator();
+int add(int a,int b){
+return a+b;
+}
+int subtract(int a,int b){
+return a-b;
+}
+
+int multiply(int a,int b){
+return a*b;
+}
+
+int division(int a,int b){
+return a/b;
+}
